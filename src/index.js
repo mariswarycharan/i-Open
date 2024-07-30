89e118078093f242ad76ec6be91f1a32db9ed5dc
Charan A A <mariswarycharan@gmail.com>
2024-07-30 15:32:12 +0530

    api update

--- src/index.js
+++ src/index.js
@@ -42,7 +42,7 @@ function AppRouter() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.post('https://i-open-roche.vercel.app', formData);
+        const response = await axios.post('https://i-open-backend.vercel.app/submit', formData);
         setResponseData(response.data);
 
         console.log(response.data);
